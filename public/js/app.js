fe46e1e630abef2677a0ac389879e156efbb0dc8
dcravenor <happy-james@hotmail.co.uk>
2018-06-06 13:42:50 +0100

    added username to comments and minor styling changes

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+console.log('Hello There, General Kenobi');
+
+// $(()=>{
+//
+//   $('.navbar-item').each(function(e) {
+//     $(this).click(function(){
+//       if($('#navbar-burger-id').hasClass('is-active')){
+//         $('#navbar-burger-id').removeClass('is-active');
+//         $('#navbar-menu-id').removeClass('is-active');
+//       }
+//     });
+//   });
+//
+//
+//   $('#navbar-burger-id').click(function () {
+//     if($('#navbar-burger-id').hasClass('is-active')){
+//       $('#navbar-burger-id').removeClass('is-active');
+//       $('#navbar-menu-id').removeClass('is-active');
+//     }else {
+//       $('#navbar-burger-id').addClass('is-active');
+//       $('#navbar-menu-id').addClass('is-active');
+//     }
+//   });
+//
+// });
